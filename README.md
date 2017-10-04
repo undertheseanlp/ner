2c65d84e7e6b7796c50874d3e7994e7ab300a80e
rain <brother.rain.1024@gmail.com>
2017-10-04 14:19:52 +0700

    update model

--- README.md
+++ README.md
@@ -2,16 +2,52 @@
 
 This repository contains experiments in Vietnamese NER problems. It is a part of [underthesea](https://github.com/magizbox/underthesea) project.
 
-## Dataset Information
+## Corpus Summary
 
-7855 tagged sentences.
+Corpus is in [UniversalDependencies format](https://github.com/UniversalDependencies/UD_Vietnamese).
+
+```
+Sentences     : 14861
+Unique words  : 18123
+Top words     : ,, ., ", của, là, một, và, có, được, người, không, đã, những, cho, ..., ở, :, trong, đến, “
+POS Tags (36) : A, Ab, B-NP, C, CH, Cb, Cc, E, FW, Fw, I, I-NP, L, M, N, NNP, NNPY, NPP, Nb, Nc, Ne, Ni, Ns, Nu, Ny, O, P, Pp, R, T, V, Vb, Vy, X, Z, p
+Chunking Tags (14) : B-AP, B-EP, B-IP, B-MP, B-NP, B-NPb, B-PP, B-VP, B-VPb, I-AP, I-NP, I-RP, I-VP, O
+NER Tags (9) :B-LOC, B-MISC, B-ORG, B-PER, I-LOC, I-MISC, I-ORG, I-PER, O
+```
 
 ## Reports
 
-* Detail Reports, [link](https://docs.google.com/spreadsheets/d/1OTd_bktaGpnLSy2I8GiFT2xhElRPymoDjPvqt4cAmc0/pubhtml?gid=0&single=true)
+![](https://img.shields.io/badge/f1-88.6%25-red.svg)
+
+* [Detail Reports](https://docs.google.com/spreadsheets/d/1OTd_bktaGpnLSy2I8GiFT2xhElRPymoDjPvqt4cAmc0/edit?usp=sharing)
+
+## Usage
+
+**Setup Environment**
+
+```
+# clone project
+$ git clone git@github.com:magizbox/underthesea.ner.git
+
+# create environment
+$ cd underthesea.ner
+$ conda create -n underthesea.ner python=3.4
+$ pip install -r requirement.txt
+```
+
+**Run experiment**
+
+```
+$ source activate underthesea.ner
+$ cd underthesea.ner
+$ python main.py
+```
 
 ## Related Works
 
-* Vietnamese NER publications, [link](https://docs.google.com/spreadsheets/d/1OTd_bktaGpnLSy2I8GiFT2xhElRPymoDjPvqt4cAmc0/pubhtml?gid=26250307&single=true)
+* [Vietnamese Named Entity Recognition Tools](https://github.com/magizbox/underthesea/wiki/Vietnamese-NLP-Tools#named-entity-recognition)
+* [Vietnamese Named Entity Recognition Publications](https://github.com/magizbox/underthesea/wiki/Vietnamese-NLP-Publications#named-entity-recognition)
+* [Vietnamese Named Entity Recognition State of The Art](https://github.com/magizbox/underthesea/wiki/Vietnamese-NLP-SOTA#named-entity-recognition)
+* [Vietnamese Named Entity Recognition Service](https://github.com/magizbox/underthesea/wiki/Vietnamese-NLP-Services#named-entity-recognition)
 
 Last update: October 2017
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from os.path import dirname, join
 from underthesea_flow.flow import Flow
 from underthesea_flow.model import Model
 from underthesea_flow.model.crf import CRF
-from underthesea_flow.reader.tagged_corpus import TaggedCorpus
 from underthesea_flow.transformer.tagged import TaggedTransformer
 from underthesea_flow.validation.validation import TrainTestSplitValidation
 
@@ -19,16 +18,15 @@ if __name__ == '__main__':
     # =========================================================================#
 
     # for evaluation
-    file = join(dirname(__file__), "corpus", "vlsp2016", "train.txt")
+    # file = join(dirname(__file__), "corpus", "vlsp2016", "train.txt")
     # file = join(dirname(__file__), "corpus", "sample_vlsp_2016", "train.txt")
-    sentences = vlsp2016.load_data(file)
+    # sentences = vlsp2016.load_data(file)
 
     # for saving model
-    # corpus_folder = join(dirname(__file__), "corpus", "vlsp_chunk")
-    # train = join(corpus_folder, "train.txt")
-    # dev = join(corpus_folder, "dev.txt")
-    # test = join(corpus_folder, "test.txt")
-    # sentences = load_data(train) + load_data(dev) + load_data(test)
+    sentences = []
+    for f in ["train.txt", "dev.txt", "test.txt"]:
+        file = join(dirname(__file__), "corpus", "vlsp2016", f)
+        sentences += vlsp2016.load_data(file)
 
     flow.data(sentences=sentences)
 
@@ -36,12 +34,19 @@ if __name__ == '__main__':
     #                                Transformer
     # =========================================================================#
     template = [
-        "T[0].lower", "T[-1].lower", "T[1].lower",
+        "T[-2].lower", "T[-1].lower", "T[0].lower", "T[1].lower", "T[2].lower",
         "T[0].istitle", "T[-1].istitle", "T[1].istitle",
-        "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",  # word unigram
-        "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]",  # word bigram
-        "T[-2][1]", "T[-1][1]", "T[0][1]", "T[1][1]", "T[2][1]",  # pos unigram,
-        "T[-2,-1][1]", "T[-1,0][1]", "T[0,1][1]", "T[1,2][1]", # pos bigram
+        # word unigram and bigram
+        "T[-2]", "T[-1]", "T[0]", "T[1]", "T[2]",
+        "T[-2,-1]", "T[-1,0]", "T[0,1]", "T[1,2]",
+        # pos unigram and bigram
+        "T[-2][1]", "T[-1][1]", "T[0][1]", "T[1][1]", "T[2][1]",
+        "T[-2,-1][1]", "T[-1,0][1]", "T[0,1][1]", "T[1,2][1]",
+        # chunk unigram and bigram
+        "T[-2][2]", "T[-1][2]", "T[0][2]", "T[1][2]", "T[2][2]",
+        "T[-2,-1][2]", "T[-1,0][2]", "T[0,1][2]", "T[1,2][2]",
+        # ner
+        "T[-3][3]", "T[-2][3]", "T[-1][3]",
     ]
     transformer = TaggedTransformer(template)
 
@@ -65,8 +70,9 @@ if __name__ == '__main__':
     flow.add_score('f1_chunk')
     flow.add_score('accuracy_chunk')
 
-    flow.set_validation(TrainTestSplitValidation(test_size=0.1))
+    # flow.set_validation(TrainTestSplitValidation(test_size=0.1))
+    flow.set_validation(TrainTestSplitValidation(test_size=0.3))
 
     flow.train()
 
-    # flow.save_model("CRF", filename="crf")
+    # flow.save_model("CRF", filename="crf_20171003.bin")
