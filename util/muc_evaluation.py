2a2a9ad6c163c97a7b9db2b96716027ae5d75bba
Vu Anh <anhv.ict91@gmail.com>
2018-05-19 11:57:19 +0700

    add techinique_report

--- util/muc_evaluation.py
+++ util/muc_evaluation.py
@@ -13,25 +13,26 @@ def muc_to_brat(muc_document):
 
     text = ""
     annotations = []
-    position = 0
+    muc_position = 0
     markers = []
-    index = re.search("<ENAMEX", muc_document)
     for match in re.finditer("<ENAMEX TYPE=\".*?\">|</ENAMEX>", muc_document):
-        start_tag, end_tag = match.regs[0]
-        tag = muc_document[start_tag:end_tag]
+        muc_start_tag, muc_end_tag = match.regs[0]
+        tag = muc_document[muc_start_tag:muc_end_tag]
+        text += muc_document[muc_position:muc_start_tag]
         # START TAG
         if tag.startswith("<ENAMEX"):
             tag_matched = re.match("<ENAMEX TYPE=\"(?P<tag>.*?)\">", tag)
             tag_name = tag_matched.group("tag")
-            markers.append((tag_name, start_tag))
+            brat_start_tag = len(text)
+            markers.append((tag_name, brat_start_tag))
         # END TAG
         else:
-            tag_name, start_position = markers.pop()
-            end_position = start_position + len(tag_name)
-            print(0)
+            tag_name, brat_start_tag = markers.pop()
+            brat_end_tag = len(text)
+            annotations.append((tag_name, brat_start_tag, brat_end_tag))
 
-        text += muc_document[position:start_tag]
-        position = end_tag
+
+        muc_position = muc_end_tag
         print("String  :", tag)
         print("Matched:", match)
     brat = BratDocument(text, annotations)
@@ -39,14 +40,16 @@ def muc_to_brat(muc_document):
 
 
 def muc_evaluation(test_files, predict_files):
-    # for test_file, predict_file in zip(test_files, predict_files):
-    #     test_muc = open(test_file).read()
-    #     brat_document = muc_to_brat(test_muc)
-    test_file, predict_file = list(zip(test_files, predict_files))[0]
-    test_muc = open(test_file).read()
-    test_brat = muc_to_brat(test_muc)
-    predict_muc = open(predict_file).read()
-    predict_brat = muc_to_brat(predict_muc)
+    count = 0
+    files = list(zip(test_files, predict_files))
+    for test_file, predict_file in files[3:]:
+        test_muc = open(test_file).read()
+        test_brat = muc_to_brat(test_muc)
+        predict_muc = open(predict_file).read()
+        predict_brat = muc_to_brat(predict_muc)
+        count += 1
+        assert test_brat.text == predict_brat.text
+    0
 
 
 if __name__ == '__main__':
