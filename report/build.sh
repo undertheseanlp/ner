5d4da8360037a7402dfdaf55ef7015c16cf7b4a5
Vu Anh <anhv.ict91@gmail.com>
2018-05-19 12:11:49 +0700

    update report

--- report/build.sh
+++ report/build.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. technique_report.tex
+name="technique_report"
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
+bibtex $name.aux
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=. $name.tex
+
+rm -rf $name.blg
+rm -rf $name.log
+rm -rf $name.out
+rm -rf $name.aux
+rm -rf $name.bbl
+rm -rf $name.synctex.gz
