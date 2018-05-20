31374e06abb4fe8a1257c6949c3c54fe6f82a778
anhv <anhv.ict91@gmail.com>
2018-05-20 10:10:25 +0700

    update technique report

--- util/error_analysis.py
+++ util/error_analysis.py
@@ -5,6 +5,7 @@ from pprint import pprint
 from sklearn.metrics import confusion_matrix
 from underthesea.util.file_io import read
 
+
 def convert_cm_to_log(cm, labels, line=5):
     cm = cm.tolist()
     # cm = [" ".join([("%-" + str(line) + "s") % labels[index]] + map(lambda i: ("%" + str(line) + "d") % i, row)) for index, row in enumerate(cm)]
@@ -15,6 +16,8 @@ def convert_cm_to_log(cm, labels, line=5):
     title = " " * (line + 1) + " ".join(map(lambda i: ("%" + str(line) + "s") % i, labels))
     cm.insert(0, title)
     return cm
+
+
 # results = json.loads(read(join("logs", "20171006_153955", "result.json")))
 results = json.loads(read(join("logs", "20171006_161437", "result.json")))
 print(0)
