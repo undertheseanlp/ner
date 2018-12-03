5ea125a09f5b2cd6be195a146245d7fb5fd4331e
anhv <anhv.ict91@gmail.com>
2018-12-03 07:59:19 +0700

    add cuda

---
+++ egs/vlsp2016_lstm/load_data.py
@@ -0,0 +1,11 @@
+def extract_tags(line):
+    texts = [text.split("\t") for text in line]
+    tokens = [text[0] for text in texts]
+    tags = [text[-1] for text in texts]
+    return tokens, tags
+
+f = open("data/train.txt")
+lines = f.read().split("\n\n")[:300]
+lines = [line.split("\n") for line in lines]
+training_data = [extract_tags(line) for line in lines]
+print(0)
